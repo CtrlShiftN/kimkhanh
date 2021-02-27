c0aa06237025fff1276ee7951c0196d5aa3189fd
binhkdc <ledangbinh18122002@gmail.com>
2021-02-27 23:01:58 +0700

    upload login, singup page Signed-off-by: binhkdc <ledangbinh18122002@gmail.com>

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -2,39 +2,72 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
+
 /* @var $model \common\models\LoginForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
+$cdnUrl = Yii::$app->params['frontend'];
 $this->title = 'Login';
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+<style>
+    .wrap {
+        background-image: url('<?= $cdnUrl ?>/img/background2.jpg');
+        background-size: cover;
+    }
+</style>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+<div class="site-login p-3">
+    <div class="row pt-5">
+        <div class="col-lg-7"></div>
+        <div class="col-lg-5 bg-light p-3 rounded">
+            <div>
+                <h1><?= Html::encode($this->title) ?></h1>
+                <p>Please fill out the following fields to Login:</p>
+                <hr>
+                <div>
+                <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                    <br>
+                <div class="float-left">
+                    <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                </div>
+                <div class="float-right">
+                    <?= Html::a('Forgot your password?', ['site/request-password-reset']) ?>.
+                </div><br>
+                <div class="mt-4 mb-3">
                     Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
                 </div>
 
                 <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= Html::submitButton('Login', ['class' => 'form-control btn btn-primary', 'name' => 'login-button']) ?>
                 </div>
-
+                </div>
+            </div>
             <?php ActiveForm::end(); ?>
+            <div class="text-center">
+                <div class="w-auto mx-auto">
+                    <div class="w-25 float-left">
+                        <hr>
+                    </div>
+                    <span>OR</span>
+                    <div class="w-25 float-right">
+                        <hr>
+                    </div>
+                </div>
+                <br>
+                <div class="mx-auto">
+                    <div class="btn btn-primary px-3"><i class="fab fa-facebook-f"></i> Facebook</div>
+                    <div class="btn btn-danger px-4"><i class="fab fa-google"></i> Google</div>
+                    <div class="btn btn-success px-4"><i class="fas fa-phone"></i> Phone</div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
--- frontend/views/site/signup.php
+++ frontend/views/site/signup.php
@@ -6,18 +6,45 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 /* @var $form ActiveForm */
+
+$cdnUrl = Yii::$app->params['frontend'];
+$this->title = 'Signup';
 ?>
-<div class="site-signup">
 
-    <?php $form = ActiveForm::begin(); ?>
-        <?= $form->field($model, 'name') ?>
-        <?= $form->field($model, 'email') ?>
-        <?= $form->field($model, 'password') ?>
-        <?= $form->field($model, 'tel') ?>
+<style>
+    .wrap {
+        background-image: url('<?= $cdnUrl ?>/img/background2.jpg');
+        background-size: cover;
+    }
+    .picture-bg {
+        background-image: url('<?= $cdnUrl ?>/img/background4.jpg');
+        background-size: cover;
+    }
+</style>
 
-    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary']) ?>
+<div class="site-signup p-3">
+    <div class="row pt-2 p-3 bg-light rounded">
+        <div class="col-lg-7 picture-bg rounded">
+
+        </div>
+        <div class="col-lg-5 border-left p-4">
+            <div class="mx-auto">
+                <h1><?= Html::encode($this->title) ?></h1>
+
+                <p>Please fill out the following fields to signup:</p>
+                <hr>
+                <div class="mb-4">
+                    <?php $form = ActiveForm::begin(); ?>
+                    <?= $form->field($model, 'name') ?>
+                    <?= $form->field($model, 'email') ?>
+                    <?= $form->field($model, 'password') ?>
+                    <?= $form->field($model, 'tel') ?>
+                </div>
+                <div class="form-group">
+                    <?= Html::submitButton(Yii::t('app', 'Signup'), ['class' => 'btn btn-primary']) ?>
+                </div>
+                <?php ActiveForm::end(); ?>
+            </div>
+        </div>
     </div>
-    <?php ActiveForm::end(); ?>
-
 </div><!-- site-signup -->
