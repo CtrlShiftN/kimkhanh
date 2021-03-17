49c4e1f57b18856aeb4cd69d19605cf867f4358b
vuvuvuvvv <nguyenvu260502@gmail.com>
2021-03-17 16:33:00 +0700

    Create contact page
    Signed-off-by: vuvuvuvvv <nguyenvu260502@gmail.com>

--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -14,48 +14,35 @@ class ContactForm extends Model
     public $email;
     public $subject;
     public $body;
-    public $verifyCode;
+    public $phone;
+    public $reason;
 
 
     /**
      * {@inheritdoc}
      */
     public function rules()
-    {
-        return [
-            // name, email, subject and body are required
-            [['name', 'email', 'subject', 'body'], 'required'],
-            // email has to be a valid email address
-            ['email', 'email'],
-            // verifyCode needs to be entered correctly
-            ['verifyCode', 'captcha'],
-        ];
+{
+    return [
+        // name, email, subject and body are required
+        [['name', 'email', 'subject', 'reason', 'body', 'phone'], 'required', 'message'=>'*Cannot be blank!',],
+        // email has to be a valid email address
+        ['email', 'email'],
+        ['phone', 'string'],
+        [['phone'], 'phoneValidate'],
+    ];
+}
+    public function phoneValidate($attribute)
+{
+    if (!preg_match('/^[0-9]{10}$/', $this->$attribute)) {
+        $this->addError($attribute, '*Phone number is invalid!');
     }
-
+}
     /**
      * {@inheritdoc}
      */
     public function attributeLabels()
-    {
-        return [
-            'verifyCode' => 'Verification Code',
-        ];
-    }
+{
 
-    /**
-     * Sends an email to the specified email address using the information collected by this model.
-     *
-     * @param string $email the target email address
-     * @return bool whether the email was sent
-     */
-    public function sendEmail($email)
-    {
-        return Yii::$app->mailer->compose()
-            ->setTo($email)
-            ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
-            ->setReplyTo([$this->email => $this->name])
-            ->setSubject($this->subject)
-            ->setTextBody($this->body)
-            ->send();
-    }
+}
 }
