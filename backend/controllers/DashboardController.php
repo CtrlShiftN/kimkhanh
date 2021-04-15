acd4888059bbfd022f9bd11f5a4bbe79f8a3800c
Doctor <karuyanakaido24@gmail.com>
2021-04-15 16:06:26 +0700

    Add backend error page layout Signed-off-by: Doctor <karuyanakaido24@gmail.com>

---
+++ backend/controllers/DashboardController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace backend\controllers;
+
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+class DashboardController extends \yii\web\Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['POST', 'GET'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+                'layout' => 'error'
+            ],
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function actionError(){
+        $this->layout = 'blank';
+        return $this->render('error');
+    }
+
+    /**
+     * @param \yii\base\Action $action
+     * @return bool
+     * @throws \yii\web\BadRequestHttpException
+     */
+    public function beforeAction($action)
+    {
+        $this->layout = 'adminlte3';
+        if (!parent::beforeAction($action)) {
+            return false;
+        }
+        return true; // or false to not run the action
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+}
